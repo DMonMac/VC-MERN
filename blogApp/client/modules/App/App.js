13ca020c271206b229edfc3c13856b55d32bac14
DMonMac <dmmacaso@gmail.com>
2017-09-18 14:45:52 +0800

    removed dev tools

--- blogApp/client/modules/App/App.js
+++ blogApp/client/modules/App/App.js
@@ -31,7 +31,6 @@ export class App extends Component {
   render() {
     return (
       <div>
-        {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
         <div>
           <Helmet
             title="MERN Starter - Blog App"
